baf4dbecb9ae7da96fbf2d2dafc700ca2a2b2258
Shoaib-malik124 <shoaibaltaf0091@gmail.com>
2026-07-07 17:33:45 +0530

    cors

--- Backend/main.py
+++ Backend/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routes.apiRoutes import router
 from slowapi import _rate_limit_exceeded_handler
 from slowapi.errors import RateLimitExceeded
@@ -8,6 +9,19 @@ from utils.limiter import limiter
 
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",       # Vite dev server
+        "http://localhost:3000",       # React dev server
+        "https://multi-agent-deep-researcher.vercel.app"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.state.limiter = limiter
 app.add_exception_handler(RateLimitExceeded,_rate_limit_exceeded_handler) # type: ignore 
 
